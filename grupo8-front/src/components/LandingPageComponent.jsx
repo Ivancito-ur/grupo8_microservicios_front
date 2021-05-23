541f6eb1955cd52ae735decf41342148950e3168
Ivan Uribe <ivanmauriciour@ufps.edu.co>
2021-05-22 20:31:10 -0500

    Agregando carrusel

--- grupo8-front/src/components/LandingPageComponent.jsx
+++ grupo8-front/src/components/LandingPageComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AuthenticatedRoute from '../Autenticacion/AuthenticatedRouter';
+import AuthenticateService from '../Autenticacion/AuthenticateService';
+import CarruselComponent from './CarruselComponent';
 import DashboardComponent from './DashboardComponent';
 import FooterComponent from './FooterComponent';
 import HeaderComponent from './HeaderComponent'
@@ -10,21 +12,19 @@ import RolesComponent from './RolesComponent';
 
 class LandingPageComponent extends Component {
     render() {
+        const authenticated = AuthenticateService.getAuthenticated();
         return (
             <div className="LandingPage">
-
                 <Router>
                     <HeaderComponent></HeaderComponent>
                     <Switch>
-                        <Route path="/login" component={LoginComponent}/>
+                        <Route path="/inicio" component={CarruselComponent} />
+                        <Route path="/login" component={LoginComponent} />
                         <AuthenticatedRoute path="/dashboard" component={DashboardComponent} />
                         <AuthenticatedRoute path="/roles" component={RolesComponent} />
                     </Switch>
                     <FooterComponent></FooterComponent>
                 </Router>
-
-                <h3>PROYECTO MICRO SERVICIOS PAGINA LANDING PAGE</h3>
-
             </div>
         );
 
--- grupo8-front/src/components/MenuComponent.jsx
+++ grupo8-front/src/components/MenuComponent.jsx
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import CarruselComponent from './CarruselComponent'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import LoginComponent from './LoginComponent';
+// import DashboardComponent from './DashboardComponent';
+// import RolesComponent from './RolesComponent';
+// import FooterComponent from './FooterComponent';
+// import HeaderComponent from './HeaderComponent'
+// import AuthenticatedRoute from '../Autenticacion/AuthenticatedRouter';
 
-class MenuComponent extends Component {
-    render() {
-        return (
-            <div className="menuComponent">
-               
-            </div>
-        );
-    }
-}
-export default MenuComponent;
+// class MenuComponent extends Component {
+//     render() {
+//         return (
+//             <div className="menuComponent">
+//                <CarruselComponent></CarruselComponent>
+//                <Router>
+//                     <HeaderComponent></HeaderComponent>
+//                     <Switch>
+//                         <Route path="/login" component={LoginComponent} />
+//                         <AuthenticatedRoute path="/dashboard" component={DashboardComponent} />
+//                         <AuthenticatedRoute path="/roles" component={RolesComponent} />
+//                     </Switch>
+//                     <FooterComponent></FooterComponent>
+
+
+//                 </Router>
+//             </div>
+//         );
+//     }
+// }
+// export default MenuComponent;
